663d92c78af556ac47ce5d97b9793a157e2fd07b
hunterviolette <hunter@maplevalleypharms.com>
2024-02-07 02:22:05 -0500

    c

--- bmb_capstone/src/pages/predictions.py
+++ bmb_capstone/src/pages/predictions.py
@@ -15,7 +15,21 @@ else:
 
 register_page(__name__, suppress_callback_exceptions=True)
 
-class Predictions():
+class DashUtil:
+  @staticmethod
+  def HTMLFormatting( className: str = 'heading', 
+                      color: str = 'info',
+                      textAlign: str = 'center'
+                    ):
+
+    if className == 'heading':
+      return f"bg-opacity-50 p-1 m-1 bg-{color} text-dark fw-bold rounded text-{textAlign}"
+    
+    elif className == 'mdiv': return {"padding": "10px"}
+    
+    else: raise Exception("className not found")
+
+class Predictions(DashUtil):
 
   dataPath = './data'
   modelPath = './models'
@@ -41,13 +55,13 @@ class Predictions():
             dcc.Input(id='diamMean', type='number', step='1', value=30),
         ]),
         dbc.Col([
-            html.H4("click three times to make predictions"),
+            html.H4("click to run predictions"),
             html.Button("click here", id="makePredictions"),
         ]),
       ], align='center'),
       html.Div(id='predictionDiv'),
 
-    ])
+    ], style=Predictions.HTMLFormatting('mdiv'))
 
   def callbacks(self):
     @callback(
@@ -76,7 +90,7 @@ class Predictions():
         img, transparent_mask = img, mask.astype(float) 
         transparent_mask[transparent_mask == 0] = np.nan
 
-        if colorscale_interp: zmin, zmax = 0, np.max(mask)+10
+        if colorscale_interp: zmin, zmax = 0, np.max(mask)+1
         else: zmin, zmax = None, None
 
         return go.Figure(go.Heatmap(z=np.where(np.isnan(transparent_mask), 
@@ -93,26 +107,32 @@ class Predictions():
 
       if clicks == None:
         return (None, None, None)
-      if clicks == 3: 
+      if clicks > 0: 
         mdiv = []
         res = Schmoo(model_dir=Predictions.modelPath, 
                     data_dir=f"{Predictions.dataPath}/{data_dir}",
                     diam_mean=diam_mean
-                  ).TestModel(model_name=model_name, debug=True)
+                  ).TestModel(model_name=model_name, debug=False)
         
 
         if isinstance(res, list):
           for re in res:
             mdiv.extend([
-                html.H3(f"Total cells found: {np.max(re[1])} for image: {re[2]}"),
+                html.H3(f"Total cells found: {np.max(re[1])} for image: {re[2]}",
+                        className=Predictions.HTMLFormatting()),
                 dbc.Row([
                     dbc.Col([
                         html.H4("Input image"),
-                        dcc.Graph(figure=TransparentImage(re[0], re[1]))
+                        dcc.Graph(figure=go.Figure(go.Heatmap(
+                          z=re[0], colorscale='greys'
+                          )).update_layout(
+                              margin=dict(l=0.1, r=0.1, t=0.1, b=0.1), 
+                              height=450, width=650
+                        ))
                     ], width=6),
                     dbc.Col([
                         html.H4("Image with predicted mask overlay"),
-                        dcc.Graph(figure=TransparentImage(re[0], re[1], 'rdbu', True))
+                        dcc.Graph(figure=TransparentImage(re[0], re[1], 'ice', True))
 
                     ], width=6), 
                 ], align='justify'),
