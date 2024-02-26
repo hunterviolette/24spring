4eecba4ee01357105c37f970948f04d369823a85
hunterviolette <hunter@maplevalleypharms.com>
2024-02-26 18:25:21 -0500

    che

--- che_capstone/balance.py
+++ che_capstone/balance.py
@@ -1,86 +1,122 @@
 import pandas as pd
 import pint
 
-uReg = pint.UnitRegistry(autoconvert_offset_to_baseunit = True)
-#uReg.default_format = "~P"
-
-uReg.define('mtpd = metric_ton / day')
-
-q = uReg.Quantity
+from chempy import Substance
 
 '''
-Reactions:
+# Electrolysis
+  MgCl2 -> Mg + Cl2
 
-N2 + 3H2 <-> 2NH3
-H2 + 1/2 O2 -> H2O
+N2 Source:
+  3Mg + N2 -> Mg3 + N2 -> Mg3N2
+
+H2 Source: 
+  3Cl2 + 3H2O -> 6HCl + 1.5O2
+  6HCL + 6NH3 -> 6NH4Cl
+
+Main rxn:
+  Mg3N2 + 6NH4Cl = 3MgCl2 + 8NH3
+
+Overall:
+  3MgCl2 + N2 + 3H2O -> 2NH3 + 1.5O2
+
 '''
 
-class Balanace:
-  def __init__(self, 
-              prod_flow_rate: float = 10.0, # in metric tonne per day
-              verbose: bool = False
-            ) -> None:
-    
-    self.verbose = verbose
-    self.prod_flow_rate = q(prod_flow_rate, 'mtpd')
-    
-    self.molec_weights = {
-      "H2": q(1.008 * 2, 'grams/mol'),
-      "O2": q(15.999 * 2, 'grams/mol'),
-      "N2": q(14.007 * 2, 'grams/mol'),
-      "H2O": q(1.008 * 2 + 15.999, 'grams/mol'),
-      "NH3": q(1.008 * 3 + 14.007, 'grams/mol'),
-      "AIR": q(28.9647, 'grams/mol') # Assumes dry air
+class Balance: 
+  def __init__(self) -> None:
+    uReg = pint.UnitRegistry(autoconvert_offset_to_baseunit = True)
+    uReg.define('mtpd = metric_ton / day')
+    self.q = uReg.Quantity
+
+    self.subs = {
+      'MgCl2': Substance.from_formula('MgCl2'),
+      'Mg': Substance.from_formula('Mg'),
+      'Cl2': Substance.from_formula('Cl2'),
+      'N2': Substance.from_formula('N2'),
+      'H2O': Substance.from_formula('H2O'),
+      'HCl': Substance.from_formula('HCl'),
+      'NH3': Substance.from_formula('NH3'),
+      'NH4Cl': Substance.from_formula('NH4Cl'),
+      'Mg3N2': Substance.from_formula('Mg3N2'),
+      'O2': Substance.from_formula('O2')
     }
 
-  def MoleFlow(self):
-    self.mol_flow = {
-      "NH3": (self.prod_flow_rate / self.molec_weights["NH3"]).to('kmol/day')
-    }
+  def OverallMaterialBalance(self):
 
-    self.mol_flow["N2"] = self.mol_flow["NH3"] / 2
-    self.mol_flow["H2"] = self.mol_flow["NH3"] / 2 * 3
-    self.mol_flow["AIR"] = self.mol_flow["N2"] / .7808
-    self.mol_flow["H2O"] = self.mol_flow["H2"]
+    targetFlow = self.q(10, 'mtpd')
+    targetMW = self.q(self.subs["NH3"].mass, 'gram/mol')
 
-    if self.verbose:
-      print(f'=== Mole Flows, basis: {self.prod_flow_rate} ===')
-      for x in self.mol_flow.keys():
-        print(f"{x} {self.mol_flow[x].__round__(2)}")
-
-  def MassFlow(self):
-    Balanace.MoleFlow(self)
+    molph = (targetFlow / targetMW).to('kmol/h')
     
-    self.mass_flow = {}
-    for key in self.mol_flow.keys():
-      self.mass_flow[key] = (self.mol_flow[key] * self.molec_weights[key]).to('kg/day')
-
-    if self.verbose:
-      print(f'=== Mass Flows ===')
-      for x in self.mass_flow.keys():
-        print(f"{x} {self.mass_flow[x].__round__(2)}")
-        
-  def NormalizedVdot(self):
-    Balanace.MassFlow(self)
+    # rxn stoich
+    ## 3 MgCl2 + N2 + 3 H2O -> 2 NH3 + 1.5 O2 + 3 MgCl2
     
-    # PV = nRT
-    p = q(1, 'atm')
-    t = q(0, 'degC').to('degK')
-    r = q(8.3144621, 'J/mol/degK')
-  
-    self.normVdot = {}
-    for key in self.mol_flow.keys():
-      self.normVdot[key] = (self.mol_flow[key] * r * t / p
-                              ).to('m**3/h').__round__(2)
+    df = pd.DataFrame()
+    for x in [[-3, "MgCl2"], [-1, "N2"], [-3, "H2O"],
+              [2, "NH3"], [1.5, "O2"], [3, "MgCl2"]]:
+      
+      mw = self.q(self.subs[x[1]].mass, 'gram/mol')
+      stoich = x[0]
 
-    print(
-      f"Hydrogen: {self.normVdot['H2']}",
-      f"Nitrogen: {self.normVdot['N2']}",
-      f"Ammonia: {self.normVdot['NH3']}",
-      f"Ammonia Mass Flow: {self.mass_flow['NH3'].to('mtpd')}",
-      sep='\n'
-    )
+      molFlow = (molph / 2 * stoich).__round__(3)
+      massFlow = (molFlow * mw).to("kg/h").__round__(3)
+
+      df = pd.concat([df,
+                      pd.DataFrame({
+                            "Component": [x[1]],
+                            "Stoch Coeff": [stoich],
+                            "Molecular Weight (grams/mol)": [mw.magnitude],
+                            "Component Flow (kmol/h)": [molFlow.magnitude],
+                            "Mass Flow (kg/h)": [massFlow.magnitude]
+                      })])
+      
+    self.ombal = df.reset_index(drop=True) # Overall material balance
+    print('=== ===',
+          f"Target: {targetFlow}",
+          f"NH3 mass per day: {(molph*targetMW).to('kg/day').__round__(4)}",
+          f"NH3 mass per hour: {(molph*targetMW).to('kg/h').__round__(4)}",
+          f"NH3 mols/h: {molph.__round__(4)}",
+          '=== Overall Material Balance ===',
+          self.ombal,
+          '',
+          sep='\n\n')
     
+  def ReactorVolume(self):
+    Balance.OverallMaterialBalance(self)
+    col = "Mass Flow (kg/h)"
+    
+    catalyst = self.q(self.ombal.at[5, col], 'kg/h')
+    nh3 = self.q(self.ombal.at[3, col] * 4, 'kg/h')
+
+    molFlow = self.q(self.ombal.at[3, 'Component Flow (kmol/h)'], 'kmol/h')
+
+    nh4cl = (molFlow * 6 / 8 * 
+              self.q(self.subs["NH4Cl"].mass, 'gram/mol')
+            ).to('kg/h')
+    
+    mg3n2 = (molFlow * 3 / 8 * 
+              self.q(self.subs["Mg3N2"].mass, 'gram/mol')
+            ).to('kg/h')
+
+    nh3syn = self.q(10**-6, 'mol/cm**2/s')
+
+    print('=== Reactor Inputs ===', 
+          f'NH4Cl (g): {nh4cl.__round__(3)}',
+          f"Mg3N2 (s): {mg3n2.__round__(3)}",
+          '=== Reactor Outputs ===',
+          f'MgCl2 (l): {catalyst}',
+          f"NH3 (g): {nh3}",
+          '=== NH3 Production ===',
+          f"NH3 (total produced): {nh3.to('mtpd').__round__(3)}",
+          f"NH3 (stored): {(nh3 * 2 / 8).to('mtpd').__round__(3)}",
+          '=== Reactor Area ===',
+          f"NH3 Synthesis: {nh3syn}",
+          f"Area (net): {(molFlow / nh3syn).to('m**2').__round__(3)}",
+          f"Area (total): {(molFlow / nh3syn * 4).to('m**2').__round__(3)}",
+          '',
+          sep='\n\n')
 
 if __name__ == "__main__":
-  Balanace(verbose=False).NormalizedVdot()
+
+  x = Balance()
+  x.ReactorVolume()
