0eba4ce70952ca120e85835d0c6b8d97148d7643
hunterviolette <hunter@maplevalleypharms.com>
2024-03-21 00:02:51 -0400

    c

--- che_capstone/src/nomics.py
+++ che_capstone/src/nomics.py
@@ -2,10 +2,11 @@ import pandas as pd
 import plotly_express as px
 from math import log2
 
-cost = 100
+cost = 3200000
+prodX = 100000
 
 df = pd.DataFrame()
-for x in range(1, 1000, 1):
+for x in range(1, prodX, 1):
   if x == 1: kn = cost
   else: kn = cost * x**log2(.8)
 
@@ -15,7 +16,10 @@ for x in range(1, 1000, 1):
               "Cost": [kn]
             })
         ])
-  
-print(df) 
 
-px.line(df, x="Module", y="Cost").show()
+df["Total Cost"] = df["Cost"].cumsum()
+
+costX = df["Total Cost"].max() / df["Total Cost"].min()
+
+print(prodX / costX, prodX, costX, sep='\n')
+#px.line(df, x="Module", y="Total Cost").show()
